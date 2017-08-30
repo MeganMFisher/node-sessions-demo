b2da9119f1b0ed6f640c83d84bc214fa357317d0
MeganMFisher <MeganMichelleFisher@gmail.com>
2017-08-30 10:12:52 -0600

    finished

--- index.js
+++ index.js
@@ -4,47 +4,42 @@ let bodyParser = require('body-parser');
 
 var app = express();
 
-//Middleware
+//MIDDLEWARE:
+
 app.use(bodyParser.json());
 app.use(session({
-    secret: 'ldkajflkejfakjelkfj;lafke',
-    resave: false,
-    saveUninitialized: false
+    secret: 'ldkajflkejfakjelkfj;lafke', //**Required** This is the secret used to sign the session ID cookie. The secret is used to encrypt the session ID, session data is stored server side always, the ID is how its matched up. 
+    resave: false, //When true it forces the session to be saved back to the session store, even if the session was never modified during the request.
+    saveUninitialized: false //When true it forces a session that is "uninitialized" to be saved to the store. A session is uninitialized when it is new but not modified.
 }))
 app.use( function(req, res, next) {
-    const { session } = req;  // const session = req.session
+    const { session } = req;  // const session = req.session. To store or access session data, you simply use the request property req.session.
     console.log(session)
-    if ( !session.user ) {
-        session.user = { username: '', favorites: []};
+    if ( !session.user ) { //Checks if there is not already a session.user
+        session.user = {  //If there isn't then create one and give it the key value pairs of 'username' with an empty string and 'favorites' with an empty array. 
+            username: '', 
+            favorites: []
+        };
     } 
-    next();
+    next(); // <- This is the trigger to indicate that our middleware was finished and to move on to the NEXT function in the chain. Remember you must call next in any middleware.
     }
 )
 
 var port = 4000
 
-var users = [{username: 'watermelon', password: '123'}]
+//ENDPOINTS:
 
 //Login 
-app.post( '/api/login', ( req, res, next ) => {
-    const { session } = req;
-    const { username, password } = req.body;
-
-    const user = users.find( user => user.username === username && user.password === password );
-
-    if ( user ) {
-        session.user.username = user.username;
-        res.status(200).send(session.user);
-    } else {
-        res.status(500).send('Unauthorized.');
-    }
-})
+app.get('/api/login', function (req, res) {
+  req.session.user.username = req.query.email; //Takes the email from the query and assigns it to the session as the username.
+  res.status(200).send(req.session.user) //Sends back the entire user object on the session.
+});
 
 
 //Get Favorites 
 app.get( '/api/getFavorites', (req, res, next) => {
         const { user } = req.session; // const favorites = req.session.user.favorites
-        res.status(200).send( user )
+        res.status(200).send( user ) //Another way to send back the entire user object is to use object destructoring above and then just send your new user variable.
     })
 
 
@@ -53,22 +48,29 @@ app.post( '/api/addFavorites', (req, res, next) => {
         const { favorites } = req.session.user; // const user = req.session.user
         let { fav } = req.query //const favorite = req.query.fav
 
-////////////////////////////////////////
-        // favorites.push(fav)
-        // res.send(favorites)
-////////////////////////////////////////
 
-        const favIndex = favorites.findIndex( favItem => favItem == fav );
+        favorites.push(fav) //We are going to take what is on the query and push it to the favorites array on the user object on the sessions.
+        res.send(req.session.user) //We will then send back the entire user object on sessions.
 
-        if ( favIndex === -1 ) {
-                favorites.push(fav)
-               return res.send(req.session.user)
-        }
-        res.status(200).send(req.session.user)
     })
 
-    //findIndex() method will return the index of an item if it is found if not it will return -1. First we check to see if the item they are trying to add to the user favorites array is already in it and if it is then we skip over the if statement and just res.send(200) with the user object on session. If not we go into the if statement below since index will return -1 and push the new item to the user favorites array.
 
+//You can use this add favorites endpoint instead if you would like it to first check if the item is already in the favorites array and if it is not then it will add it to the array. Uncomment out the full endpoint to see the code and comments. 
+
+
+// app.post( '/api/addFavorites', (req, res, next) => {
+//         const { favorites } = req.session.user; // const user = req.session.user
+//         let { fav } = req.query //const favorite = req.query.fav
+
+        
+//         const favIndex = favorites.findIndex( favItem => favItem == fav ); //We are using the .findIndex() method which will search through our favorites array on sessions to see if the fav item from our query is already in the array. If it is findIndex will return the index of the item if it is not findIndex will return -1.
+
+//         if ( favIndex === -1 ) { //If we did receive -1 from findIndex() we now now the item is not already in the array and needs to be added. 
+//                 favorites.push(fav) //We push the new item to the favorites array on the sessions.
+//                return res.send(req.session.user) //We will then send back the entire user object on sessions.
+//         }
+//         res.status(200).send(req.session.user) //If we already have that item in the array we will just send back the entire user object on sessions.
+// })
 
 
 app.listen( port, () => { console.log(`Server listening on port ${port}.`); } );
